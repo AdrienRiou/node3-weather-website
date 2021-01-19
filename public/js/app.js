cd644ff29a1519b181cd730afe8393ece7502844
Adrien Riou <adrien.riou@vo2-group.com>
2021-01-19 14:26:45 -0500

    FIX

--- public/js/app.js
+++ public/js/app.js
@@ -19,7 +19,7 @@ weatherForm.addEventListener('submit', (e) => {
             if(data.error) {
                 messageOne.textContent = data.error;
             } else {
-                messageOne.textContent = data.location;
+                messageOne.textContent = 'Salut Polly je te fais des bisous';
                 messageTwo.textContent = JSON.stringify(data.forecast);
             }
         })
